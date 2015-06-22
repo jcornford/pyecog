47f480bb2273c6be93ded4335a8d28d9639f204d
JCornford <jonathan.cornford@gmail.com>
2015-06-22 14:01:50 +0100

    update standardisation

---
+++ pythoncode/classifiers/neighbors.py
@@ -0,0 +1,39 @@
+from sklearn import neighbors
+class KNeighbors():
+    """
+    Support vector classifier
+    """
+    def __init__(self, n_neighbors = 15, weights = 'uniform'):
+        self.classifier = neighbors.KNeighborsClassifier(n_neighbors, weights=weights)
+    def fit(self, X, y):
+        """
+        Method to fit the model.
+        
+        Parameters:
+        X : 2d numpy array of training data, X.shape = [n_samples, d_features]
+        y : 1d numpy array of training labels
+        """
+        self.classifier = self.classifier.fit(X, y)
+        
+
+    def predict(self, X):
+        """
+        Method to apply the model data
+        
+        Parameters:
+        X : 2d numpy array of test data
+        """
+        return self.classifier.predict(X)
+    
+    def test(self, X, y):
+        """
+        Method to apply the model data
+        
+        Parameters:
+        X : 2d numpy array of test data
+        y : actual labels
+        """
+        # [:, 1] to get the second column, which contains the probabilies of 
+        # of class being 1
+        score  =self.classifier.score(X,y)
+        return score
---
+++ pythoncode/extractors/waveletfeatures.py
@@ -0,0 +1,90 @@
+import numpy as np
+import featureBaseClass
+import scipy.stats as st
+import sklearn
+
+class WaveletFeatures():
+    """
+    Extracting frequency features using wavelets
+
+    """
+
+    def __init__(self):
+        pass
+
+    def extract(self,data):
+        power = []
+        self.myWaveletFamily, self.frequencies = self._morletFamily(start=1,stop=50,step = 4)
+        for i in range(data.shape[0]):
+            self.convResults = self._convolve(data[i,:], self.myWaveletFamily,self.frequencies, fs = 512)
+            powerArray  = np.absolute(self.convResults)
+            power.append(powerArray)
+        self.max_wave_powers = np.vstack([np.max(power[i], axis = 1) for i in range(len(power))])
+        self.names = self.frequencies
+        return self.max_wave_powers
+
+
+    def _convolve(self,dataSection, wavelets,frequencies, fs):
+        try:
+            dataSection=dataSection[:,0]
+        except:
+            pass
+
+        convResults = []
+
+        for i in range(len(wavelets)):
+            wavelet = wavelets[i]
+            complexResult = np.convolve(dataSection,wavelet,'same')
+            convResults.append(complexResult) 
+
+        convResults = np.array(convResults) # convert to a numpy array!
+        return convResults
+
+    def _complexMorlet(self,freq, samplingFreq, timewindow = (-0.1,0.1), noWaveletCycles=6,
+                    freqNorm = False,
+                    plotWavelet = False):
+        '''
+        See above markdown cell for formula ;)
+        
+        freq              = wavelet frequency
+        samplingFrequency = 'insert data sampling frequency here'
+        noWaveletCycles   = 'reread variable name' - trades off temp and freq precision
+        timewindow        = timewindow over which to calculate wavelet
+        freqNorm          = Bool, decide whether to normlise over frequencies
+        plotWavelet       = Plot the construction?
+        '''
+
+        n = noWaveletCycles
+        f = freq
+        timeaxis = np.linspace(timewindow[0],timewindow[1],samplingFreq*(2.*timewindow[1]))
+        sine_wave = np.exp(2*np.pi*1j*f*timeaxis)
+        gstd = n/(2.*np.pi*f)
+        gauss_win = np.exp(-timeaxis**2./(2.*gstd**2.))
+        
+        A = 1 # just 1 if not normlising freqs
+        if freqNorm:
+            #gstd    = (4/(2*np.pi*f))**2# this is his?
+            A = 1.0/np.sqrt((gstd*np.sqrt(np.pi)))
+            print 'freqNorm is :',A,' for', f, ' Hz'
+        
+        wavelet = A*sine_wave*gauss_win
+        #print wavelet.shape
+            
+        return wavelet, timeaxis
+
+            
+    def _morletFamily(self,start=1,stop=50,timewindow = (-0.1,0.1),step = 5,fs = 512.0, freqNorm = False):
+        '''
+        Returns a list of wavelets 
+        Inputs:
+        start, stop and step of the frequencies
+        fs is sampling freq - should equal data's
+        freqNorm - to normlise over frequencies?
+        '''
+        waveletList = []
+        frequencies = []
+        for f in np.arange(start,stop,step):
+            wavelet, xaxis = self._complexMorlet(f,fs,timewindow ,freqNorm=freqNorm)
+            waveletList.append(wavelet)
+            frequencies.append(f)
+        return waveletList,frequencies
