2721dc650725d58cad2dd758cfe72822dc6d7e0a
jcornford <jonathan.cornford@gmail.com>
2015-12-01 13:33:29 +0000

    update extraction

--- pythoncode/nc.py
+++ pythoncode/nc.py
@@ -1,19 +1,156 @@
 import time
+import sys
+
 import pickle
 import numpy as np
+import scipy.stats as st
 import matplotlib.pyplot as plt
+
+
+from numpy import NaN, Inf, arange, isscalar, asarray, array # imports for the peakdet method
 
 
 class FeatureExtractor():
 
     def __init__(self, dataset):
+        self.start = time.clock()
         self.dataset = dataset
+        self._baseline(subtract_baseline=True, threshold=0.04, window_size=100)
+        self._event_dataset_stats()
+        self._peaks_and_valleys()
+        self._wavelet_features(fs = 512, frequencies = [1,5,10,15,20,30,60, 90])
 
-        window_size = 100
-        array_std = np.std(self.rolling_window(self.dataset,window=window_size),-1)
-        array_window = np.zeros([self.dataset.shape[0],window_size-1])
+        print("Stacking up..."),
+        self.feature_array = np.hstack([self.event_stats,self.baseline_features,
+                                   self.pkval_stats,self.meanpower])
+        print 'DONE!'
+
+        print self.feature_array.shape
+        print 'took ', time.clock()-self.start, ' seconds to extract ', dataset.shape[0], 'feature vectors'
+
+
+    def _event_dataset_stats(self):
+        print("Extracting stats on event dataset..."),
+        self.event_stats = np.zeros((len(self.event_dataset),7))
+        self.event_stats_col_labels = ('min','max','mean','std-dev','skew','kurtosis','sum(abs(difference))')
+
+        for i,trace in enumerate(self.event_dataset):
+            if trace.shape[0]:
+                # if not all baseline
+                self.event_stats[i,0] = np.min(trace)
+                self.event_stats[i,1] = np.max(trace)
+                self.event_stats[i,2] = np.mean(trace)
+                self.event_stats[i,3] = np.std(trace)
+                self.event_stats[i,4] = st.kurtosis(trace)
+                self.event_stats[i,5] = st.skew(trace)
+                self.event_stats[i,6] = np.sum(np.absolute(np.diff(trace)))
+            else:
+                 # if all baseline
+                self.event_stats[i,:] = np.NaN
+
+        print "DONE", time.clock() - self.start
+
+    def _baseline(self, subtract_baseline = True, threshold = 0.04, window_size = 100):
+        '''
+        Method calculates 2 feature lists and an event dataset:
+            - self.event_dataset is the data window with 'baseline points removed'
+            - baseline_length is the first feature list and is the number of datapoints within the window
+              defined as being baseline using a rolling std deviation window and threshold
+            - baseline_mean_diff is the second list and is the mean difference between baseline datapoint indexes.
+        '''
+        print 'Extracting baseline via rolling std...',
+        array_std = np.std(self.rolling_window(dataset,window=window_size),-1)
+        array_window = np.zeros([dataset.shape[0],window_size-1])
         rolling_std_array = np.hstack((array_window,array_std))
-        masked_std = np.ma.masked_where(rolling_std_array > threshold, rolling_std_array)
+        masked_std_below_threshold = np.ma.masked_where(rolling_std_array > threshold, dataset)
+        mean_baseline_vector = np.mean(masked_std_below_threshold,axis = 1)
+
+        if subtract_baseline:
+            dataset_after_subtraction_option = dataset - mean_baseline_vector[:,None]
+
+        masked_std_above_threshold = np.ma.masked_where(rolling_std_array < threshold, dataset_after_subtraction_option)
+        indexes = np.array(np.arange(5120))
+
+        self.event_dataset = [np.ma.compressed(masked_std_above_threshold[i,:]) for i in xrange(dataset.shape[0])]
+        baseline_length = [len(np.ma.compressed(masked_std_below_threshold[i,:])) for i in xrange(dataset.shape[0])]
+        baseline_mean_diff = [np.mean(np.diff(indexes[np.logical_not(masked_std_below_threshold[i].mask)])) for i in xrange(dataset.shape[0])]
+        #self.baseline_diff = [indexes[np.logical_not(masked_std_below_threshold[i].mask)] for i in xrange(dataset.shape[0])]
+
+        self.baseline_features = np.vstack([baseline_length,baseline_mean_diff]).T
+        print 'DONE', time.clock() - self.start
+        print 'N.B using mean baseline difference - perhaps not best?'
+
+    def _peaks_and_valleys(self):
+        print ("Extracting peak/valley features..."),
+        pk_nlist = []
+        val_nlist = []
+
+        for i in range(dataset.shape[0]):
+            pk, val = self.peakdet(dataset[i,:], 0.5)
+            pk_nlist.append(pk)
+            val_nlist.append(val)
+
+        # always 2 columns
+        n_pks = []
+        n_vals = []
+        av_pk = []
+        av_val = []
+        av_range = []
+        for i in range(len(pk_nlist)):
+            n_pks.append(pk_nlist[i].shape[0])
+            n_vals.append(val_nlist[i].shape[0])
+
+            if pk_nlist[i].shape[0]:
+                av_pk.append(np.mean(pk_nlist[i][:,1]))
+            else:
+                av_pk.append(np.NaN)
+
+            if val_nlist[i].shape[0]:
+                av_val.append(np.mean(val_nlist[i][:,1]))
+            else:
+                av_val.append(np.NaN)
+
+            if val_nlist[i].shape[0] and pk_nlist[i].shape[0]:
+                av_range.append(
+                    abs(np.mean(pk_nlist[i][:,1])+np.mean(val_nlist[i][:,1]))
+                    )
+            else:
+                av_range.append(np.NaN)
+
+        n_pks = np.array(n_pks)
+        n_vals = np.array(n_vals)
+        av_pk = np.array(av_pk)
+        av_val = np.array(av_val)
+        av_range = np.array(av_range)
+        self.pkval_stats = np.vstack([n_pks,n_vals,av_pk,av_val,av_range]).T
+        print("DONE", time.clock()-self.start)
+        print ('N.B Again - preassign?')
+
+    def _wavelet_features(self, fs = 512, frequencies = [1,5,10,15,20,30,60, 90]):
+        print("Extracting wavelet features from event dataset..."),
+        window = int(fs*0.5)
+        waveletList = []
+        for freq in frequencies:
+            wavelet, xaxis = self.complexMorlet(freq,fs,timewindow = (-0.1,0.1))
+            waveletList.append(wavelet)
+
+        power = []
+        meanpower = []
+        for i in range(len(self.event_dataset)):
+            if self.event_dataset[i].shape[0]:
+                convResults = self.convolve(self.event_dataset[i], waveletList)
+                powerArray  = np.absolute(convResults)
+                #print powerArray.shape
+                power.append(powerArray)
+                meanpower.append(np.mean(powerArray,axis = 1))
+
+            else:
+                power.append(np.ones((len(frequencies)))*np.NaN)
+                meanpower.append(np.ones((len(frequencies)))*np.NaN)
+        self.meanpower = np.array(meanpower)
+        print("DONE", time.clock()-self.start)
+
+        print("N.B. Still no post crossing power")
 
     @ staticmethod
     def rolling_window(array, window):
@@ -24,89 +161,8 @@ class FeatureExtractor():
         strides = array.strides + (array.strides[-1],)
         return np.lib.stride_tricks.as_strided(array, shape=shape, strides=strides)
 
-
-
-def rolling_std(X, w):
-
-    print time.clock(),'rolling std start'
-    r = np.empty(X.shape)
-    r.fill(np.nan)
-    for i in range(w - 1, X.shape[0]):
-        r[i] = np.std(X[(i-w+1):i+1])
-    print time.clock(),'rolling std end'
-    return r
-
-def rolling_window(array, window):
-    shape = array.shape[:-1] + (array.shape[-1] - window + 1, window)
-    strides = array.strides + (array.strides[-1],)
-    return np.lib.stride_tricks.as_strided(array, shape=shape, strides=strides)
-
-def extract_features(dataset):
-
-
-    baseline_point_subtraction = True
-    start  = time.clock()
-    threshold = 0.04
-    window_size = 100
-    array_std = np.std(rolling_window(dataset,window=window_size),-1)
-    array_window = np.zeros([dataset.shape[0],window_size-1])
-    rolling_std_array = np.hstack((array_window,array_std))
-    masked_std_below_threshold = np.ma.masked_where(rolling_std_array > threshold, dataset)
-
-    mean_baseline_vector = np.mean(masked_std_below_threshold,axis = 1)
-    if baseline_point_subtraction:
-        dataset_after_subtraction_option = dataset - mean_baseline_vector[:,None]
-    masked_std_above_threshold = np.ma.masked_where(rolling_std_array < threshold, dataset_after_subtraction_option)
-    event_dataset = [np.ma.compressed(masked_std_above_threshold[i,:]) for i in xrange(dataset.shape[0])]
-
-    baseline_length = [len(np.ma.compressed(masked_std_below_threshold[i,:])) for i in xrange(dataset.shape[0])]
-
-    indexes = np.array(np.arange(5120))
-    #baseline_diff = [indexes[np.logical_not(masked_std_below_threshold[i].mask)] for i in xrange(dataset.shape[0])]
-    baseline_mean_diff = [np.mean(np.diff(indexes[np.logical_not(masked_std_below_threshold[i].mask)])) for i in xrange(dataset.shape[0])]
-
-
-    #Now for the stats on the event dataset
-    print("Extracting stats on event dataset..."),
-    import scipy.stats as st
-    min_ = []
-    max_ = []
-    mean_ = []
-    std_ = []
-    skew_ = []
-    kurtosis_ = []
-    diff_sum_ = []
-
-    for trace in event_dataset:
-        if trace.shape[0]:
-            min_.append(np.min(trace))
-            max_.append(np.max(trace))
-            mean_.append(np.mean(trace))
-            std_.append(np.std(trace))
-            kurtosis_.append(st.kurtosis(trace))
-            skew_.append(st.skew(trace))
-            diff_sum_.append(np.sum(np.absolute(np.diff(trace))))
-        else:
-            min_.append(np.NaN)
-            max_.append(np.NaN)
-            mean_.append(np.NaN)
-            std_.append(np.NaN)
-            kurtosis_.append(np.NaN)
-            skew_.append(np.NaN)
-            diff_sum_.append(np.NaN)
-
-    min_ = np.array(min_)
-    max_ = np.array(max_)
-    mean_ = np.array(mean_)
-    std_ = np.array(std_)
-    skew_ = np.array(skew_)
-    kurtosis_ = np.array(kurtosis_)
-    diff_sum_ = np.array(diff_sum_)
-    print("DONE")
-
-    print ("Extracting peak/valley features..."),
-    from numpy import NaN, Inf, arange, isscalar, asarray, array
-    def peakdet(v, delta, x = None):
+    @staticmethod
+    def peakdet(v, delta, x=None):
         """
         function [maxtab, mintab]=peakdet(v, delta, x)
                 [MAXTAB, MINTAB] = PEAKDET(V, DELTA) finds the local
@@ -158,50 +214,7 @@ def extract_features(dataset):
                     lookformax = True
         return array(maxtab), array(mintab)
 
-    pk_nlist = []
-    val_nlist = []
-    for i in range(dataset.shape[0]):
-        pk, val = peakdet(dataset[i,:], 0.5)
-        pk_nlist.append(pk)
-        val_nlist.append(val)
-
-    # always 2 columns
-    n_pks = []
-    n_vals = []
-    av_pk = []
-    av_val = []
-    av_range = []
-    for i in range(len(pk_nlist)):
-        n_pks.append(pk_nlist[i].shape[0])
-        n_vals.append(val_nlist[i].shape[0])
-
-        if pk_nlist[i].shape[0]:
-            av_pk.append(np.mean(pk_nlist[i][:,1]))
-        else:
-            av_pk.append(np.NaN)
-
-        if val_nlist[i].shape[0]:
-            av_val.append(np.mean(val_nlist[i][:,1]))
-        else:
-            av_val.append(np.NaN)
-
-        if val_nlist[i].shape[0] and pk_nlist[i].shape[0]:
-            av_range.append(
-                abs(np.mean(pk_nlist[i][:,1])+np.mean(val_nlist[i][:,1]))
-                )
-        else:
-            av_range.append(np.NaN)
-
-
-
-    n_pks = np.array(n_pks)
-    n_vals = np.array(n_vals)
-    av_pk = np.array(av_pk)
-    av_val = np.array(av_val)
-    av_range = np.array(av_range)
-    print("DONE")
-
-    print("Extracting wavelet features from event dataset..."),
+    @staticmethod
     def complexMorlet(freq, samplingFreq, timewindow,
                   noWaveletCycles=6,freqNorm = False):
         '''
@@ -212,7 +225,6 @@ def extract_features(dataset):
         freqNorm          = Bool, decide whether to normlise over frequencies
         plotWavelet       = Plot the construction?
         '''
-
         n = noWaveletCycles
         f = freq
         timeaxis = np.linspace(timewindow[0],timewindow[1],samplingFreq*(2.*timewindow[1]))
@@ -231,6 +243,7 @@ def extract_features(dataset):
 
         return wavelet, timeaxis
 
+    @staticmethod
     def convolve(dataSection, wavelets):
         convResults = []
         for i in range(len(wavelets)):
@@ -242,56 +255,13 @@ def extract_features(dataset):
         #print convResults.shape
         return convResults
 
-    fs = 512
-    window = int(fs*0.5)
 
 
-    waveletList = []
-    frequencies = [1,5,10,15,20,30,60, 90]
-    for freq in frequencies:
-        wavelet, xaxis = complexMorlet(freq,fs,timewindow = (-0.1,0.1))
-        waveletList.append(wavelet)
 
-    power = []
-    meanpower = []
-    for i in range(len(event_dataset)):
-        if event_dataset[i].shape[0]:
-            convResults = convolve(event_dataset[i], waveletList)
-            powerArray  = np.absolute(convResults)
-            #print powerArray.shape
-            power.append(powerArray)
-            meanpower.append(np.mean(powerArray,axis = 1))
 
-        else:
-            power.append(np.ones((len(frequencies)))*np.NaN)
-            meanpower.append(np.ones((len(frequencies)))*np.NaN)
-    meanpower = np.array(meanpower)
-    print("DONE")
-
-    print("Really need post crossing power")
-
-    print("Stacking up...")
-    event_stats = np.vstack([min_,max_,mean_,skew_,std_,kurtosis_,diff_sum_]).T
-    print event_stats.shape
-    baseline_features = np.vstack([baseline_length,baseline_mean_diff]).T
-    print baseline_features.shape
-    pkval_stats = np.vstack([n_pks,n_vals,av_pk,av_val,av_range]).T
-    print pkval_stats.shape
-
-    print meanpower.shape
-
-    feature_array = np.hstack([event_stats,baseline_features,
-                               pkval_stats,meanpower])
-    print feature_array.shape
-    print 'DONE!',
-    print 'took ', time.clock()-start, ' seconds'
-    return feature_array
-start = time.clock()
 dataset = pickle.load(open('../dataset','rb'))
-print time.clock()-start, 'seconds to load - now running extraction'
-start2 = time.clock()
-extract_features(dataset)
-print time.clock()-start2, 'finished extraction'
+extractor = FeatureExtractor(dataset)
+
 
 # begin piss-around
 '''
