13b2256b78efe78fb55e869f80b05270d1bac8ad
jcornford <jonathan.cornford@gmail.com>
2015-12-09 09:43:01 +0000

    setting up timestamp & percentage state code

--- pythoncode/network_predictor.py
+++ pythoncode/network_predictor.py
@@ -2,23 +2,28 @@
 from network_loader import SeizureData
 from extrator import FeatureExtractor
 
+import pickle
 import numpy as np
-class predictor():
+class Predictor():
 
     def __init__(self, clf_pickle_path = None):
         if clf_pickle_path == None:
             clf_pickle_path = '../saved_clf'
         self.classifier = pickle.load(open(clf_pickle_path,'rb'))
         self.r_forest = self.classifier.r_forest
+        print self.r_forest
 
-cleanup = np.loadtxt('../Training_cleanup.csv',delimiter=',')
-labels = np.array([int(x[1]) for x in cleanup])
-print labels
-ok_indexes = []
-for i in range(labels.shape[0]):
-        if labels[i] != 0:
-            ok_indexes.append(i)
-print labels[ok_indexes]
+pr = Predictor()
+
+
+#cleanup = np.loadtxt('../Training_cleanup.csv',delimiter=',')
+#labels = np.array([int(x[1]) for x in cleanup])
+#print labels
+#ok_indexes = []
+#for i in range(labels.shape[0]):
+#        if labels[i] != 0:
+#            ok_indexes.append(i)
+#print labels[ok_indexes]
     #validation_data.feature_array_fair = validation_data.feature_array[ok_indexes,:]
     #validation_labels_fair = val_labels[ok_indexes]
 
