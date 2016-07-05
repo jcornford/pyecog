9ca5820825943ed909731216b099db128f7e82da
jcornford <jonathan.cornford@gmail.com>
2016-07-05 16:23:40 +0100

    starting to think about pip

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pyecog',
+  packages = ['pyecog', 'pyecog.ndf','pyecog.light_code', 'pyecog.visualisation'], # this must be the same as the name above
+  version = '0.1',
+  description = 'For classifying single channel ECoG recordings (iEEG)',
+  author = 'Jonathan Cornford',
+  author_email = 'jonathan.cornford@gmail.com',
+  url = 'https://github.com/jcornford/pyecog', # use the URL to the github repo
+  download_url = 'https://github.com/jcornford/pyecog/tarball/0.1', # I'll explain this in a second
+  keywords = ['iEEG', 'ECoG'], # arbitrary keywords
+  install_requires=['numpy', 'pandas', 'scipy', 'matplotlib', 'seaborn','yahmm','h5py','xlrd']
+)
