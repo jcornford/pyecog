- bug fixes for filtering - added scaling for prediction stuff
- commiting before changing the way data is sampled... and using class weights

pyecog/ndf/classifier.py
@@ -111,7 +111,7 @@ class Classifier():
         print(metrics.classification_report(np.ravel(self.labels),self.predictions))
         '''
 
-    def estimate_clf_error(self, nfolds = 5):
+    def estimate_clf_error(self, nfolds = 3):
         #self.printProgress(0,nfolds, prefix = 'Cross validation:', suffix = 'Complete', barLength = 50)
         #fold = 1
         print ('Running '+str(nfolds)+'-fold cross validation to estimate classifier performance:')
@@ -126,11 +126,14 @@ class Classifier():
             y_test  = self.labels[test_ix]
 
             y_counts = pd.Series(y_train).value_counts().values
-            target_resample = (y_counts[1]*50,y_counts[1]*3)
-            samp_y, samp_x = self.resample_training_dataset(y_train, X_train, sizes = target_resample)
-            rf = RandomForestClassifier(n_jobs=-1, n_estimators= 500, oob_score=True, bootstrap=True)
-            rf.fit(samp_x, np.ravel(samp_y))
-
+            #target_resample = (y_counts[1]*50,y_counts[1]*3)
+            #samp_y, samp_x = self.resample_training_dataset(y_train, X_train, sizes = target_resample)
+            print('running balanced, no sampling')
+            rf = RandomForestClassifier(n_jobs=-1, n_estimators= 500,
+                                        oob_score=True, bootstrap=True,
+                                        class_weight= 'balanced')
+            #rf.fit(samp_x, np.ravel(samp_y))
+            rf.fit(X_train, np.ravel(y_train))
             # now you need the hmm params! nest this shit up
             train_emission_probs = self.get_cross_validation_emission_probs(X_train, y_train,nfolds = 4)
             train_transition_probs = hmm.get_state_transition_probs(y_train)
@@ -161,9 +164,9 @@ class Classifier():
                 map_recall.append(recall_score)
                 f1_score = metrics.f1_score(y_test, fb)
                 map_f1.append(f1_score)
-                print ('fb Precision: '+ str(p_score))
-                print ('fb Recall: '+ str(recall_score))
-                print ('fb F1: '+ str(f1_score))
+                #print ('fb Precision: '+ str(p_score))
+                #print ('fb Recall: '+ str(recall_score))
+                #print ('fb F1: '+ str(f1_score))
                 fb = True
             except:
                 fb = False

pyecog/ndf/datahandler.py
@@ -75,11 +75,7 @@ apply_async_with_callback()
     def __init__(self, logpath = os.getcwd()):
 
         self.parallel_savedir = None
-        #print(os.getcwd())
-        #global loggers
-        #if loggers.get(name):
-        #    return loggers.get(name)
-        #else:
+
 
     def add_features_seizure_library(self,
                                     libary_path,
@@ -109,11 +105,9 @@ apply_async_with_callback()
                     data_array = group['data'][:]
                     assert len(data_array.shape) > 1
 
-                    fdata = filterArray(data_array, window_size= filter_window, order= filter_order)
-                    #fndata = self._normalise(fdata)
-                    fndata = fdata
-                    if fndata is not None:
-                        extractor = FeatureExtractor(fndata, fs = group.attrs['fs'], verbose_flag = False)
+                    #fdata = filterArray(data_array, window_size= filter_window, order= filter_order)
+                    if data_array is not None:
+                        extractor = FeatureExtractor(data_array, fs = group.attrs['fs'], verbose_flag = False)
                         features = extractor.feature_array
 
                         try:
@@ -160,6 +154,15 @@ apply_async_with_callback()
 
                 data_array = tid['data'][:]
                 if len(data_array.shape) == 1:
+
+
+                    logging.debug('Centering, high pass filtering and scaling data to mode std:')
+                    data_array = self.highpassfilter_and_standardise(data_array, tid.attrs['fs'])
+                    if data_array is None:
+                        logging.error(' ERROR: File has no 5 seconds without 0 std, all the same')
+                        logging.error("Didn't add features to group: "+ str(tid))
+                        ERROR_FLAG = True
+                        return 0
                     logging.debug('Reshaping data from '+str(data_array.shape)+ ' using '+str(tid.attrs['fs']) +' fs' +
                                   ' and timewindow of '+ str(timewindow)  )
                     data_array = self._make_array_from_data(data_array, fs = tid.attrs['fs'], timewindow = timewindow)
@@ -172,10 +175,11 @@ apply_async_with_callback()
                         print('Data file does not contain any data. Exiting: ')
                         return 0
 
-                fdata = filterArray(data_array, window_size= filter_window, order= filter_order)
-                fndata = self._normalise(fdata)
-                if fndata is not None:
-                    extractor = FeatureExtractor(fndata,tid.attrs['fs'], verbose_flag = False)
+                #fdata = filterArray(data_array, window_size= filter_window, order= filter_order)
+                if data_array is not None:
+                    #for row in range(data_array.shape[0]):
+                        #print(data_array[row, :])
+                    extractor = FeatureExtractor(data_array,tid.attrs['fs'], verbose_flag = False)
                     features = extractor.feature_array
 
                     try:
@@ -409,9 +413,16 @@ apply_async_with_callback()
         # now standardise
         reshaped = np.reshape(filtered_data, (int(3600/stdtw), int(stdtw*fs)))
         std_vector = np.round(np.std(reshaped, axis = 1),decimals=std_dec_places)
-        mode_std = stats.mode(std_vector)[0]
-        scaled = np.divide(data,mode_std)
-        return scaled
+        std_vector = std_vector[std_vector != 0]
+        if std_vector.shape[0] > 0:
+            mode_std = stats.mode(std_vector)[0] # can be zero if there is big signal loss
+            logging.debug(str(mode_std)+' is mode std of trace split into '+ str(stdtw)+ ' second chunks')
+            scaled = np.divide(filtered_data, mode_std)
+            return scaled
+        elif std_vector.shape[0] == 0:
+            logging.error(' File std is all 0, returning None')
+            return None
+
 
     @staticmethod
     def _make_array_from_data(data, fs, timewindow):
@@ -443,7 +454,8 @@ apply_async_with_callback()
             ERROR_flag = True
             #return None
 
-    def convert_ndf_directory_to_h5(self, ndf_dir, tids = 'all', save_dir  = 'same_level', n_cores = -1, fs = 'auto'):
+    def convert_ndf_directory_to_h5(self, ndf_dir, tids = 'all', save_dir  = 'same_level', n_cores = -1, fs = 'auto',
+                                    scale_and_filter = False):
         """
 
         Args:
@@ -457,6 +469,7 @@ apply_async_with_callback()
 
         """
         global ERROR_FLAG
+        self.scale_and_filter = scale_and_filter
         self.fs_for_parallel_conversion = fs
         files = [f for f in self._fullpath_listdir(ndf_dir) if f.endswith('.ndf')]
 
@@ -465,12 +478,6 @@ apply_async_with_callback()
         if not tids == 'all':
             if not hasattr(tids, '__iter__'):
                 tids = [tids]
-            #for tid in tids:
-            #    try:
-            #        assert tid in ndf.tid_set
-            #    except AssertionError:
-            #        print('Please enter valid tid (at least for the first!) file in directory ('+str(ndf.tid_set)+')')
-            #        sys.exit()
 
         self.tids_for_parallel_conversion = tids
         print ('Transmitters for conversion: '+ str(self.tids_for_parallel_conversion))
@@ -502,6 +509,7 @@ apply_async_with_callback()
         savedir = self.savedir_for_parallel_conversion
         tids = self.tids_for_parallel_conversion
         fs = self.fs_for_parallel_conversion
+        scale_and_filter_flag = self.scale_and_filter
 
         # convert m name
         mname = os.path.split(filename)[1]
@@ -512,7 +520,7 @@ apply_async_with_callback()
         try:
             ndf = NdfFile(filename, fs = fs)
             if set(tids).issubset(ndf.tid_set) or tids == 'all':
-                ndf.load(tids)
+                ndf.load(tids, scale_and_filter = scale_and_filter_flag)
                 abs_savename = os.path.join(savedir, filename.split('/')[-1][:-4]+ndf_time+' tids_'+str(tids))
                 ndf.save(save_file_name= abs_savename)
             else:

pyecog/ndf/ndfconverter.py
@@ -9,6 +9,7 @@ import h5py
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.stats as ss
+from scipy import signal, stats
 #from line_profiler import LineProfiler
 
 
@@ -395,7 +396,8 @@ class NdfFile:
 
     def load(self, read_ids = [],
              auto_glitch_removal = True,
-             auto_resampling = True,):
+             auto_resampling = True,
+             scale_and_filter = False):
 
         self.read_ids = read_ids
         logging.info('Loading '+ self.filepath +'read ids are: '+str(self.read_ids))
@@ -423,7 +425,6 @@ class NdfFile:
             assert read_id in self.tid_set, "Transmitter %i is not a valid transmitter id" % read_id
             self.tid_raw_data_time_dict[read_id]['data'] = self.voltage_messages[self.transmitter_id_bytes == read_id] * self.volt_div
             self.tid_raw_data_time_dict[read_id]['time'] = self.time_array[self.transmitter_id_bytes == read_id]
-
         self._correct_bad_messages()
 
         if auto_glitch_removal:
@@ -431,6 +432,33 @@ class NdfFile:
         if auto_resampling:
             self.correct_sampling_frequency()
             # there should now be no nans running through here!
+        if scale_and_filter:
+            for tid in self.read_ids:
+                self.tid_data_time_dict[tid]['data'] = self.highpassfilter_and_standardise(self.tid_data_time_dict[tid]['data'], fs = self.tid_to_fs_dict[tid] )
+
+
+    @staticmethod
+    def highpassfilter_and_standardise(data, fs, cutoff_hz = 1, stdtw = 5,std_dec_places = 2):
+        logging.debug('Highpassfiltering and standising mode std, fs: ' + str(fs))
+
+        nyq = 0.5 * fs
+        cutoff_hz = cutoff_hz/nyq
+        data = data-np.mean(data)
+        b, a = signal.butter(2, cutoff_hz, 'highpass', analog=False)
+        filtered_data = signal.filtfilt(b,a, data, padtype=None)
+
+        # now standardise
+        reshaped = np.reshape(filtered_data, (int(3600/stdtw), int(stdtw*fs)))
+        std_vector = np.round(np.std(reshaped, axis = 1),decimals=std_dec_places)
+        std_vector = std_vector[std_vector != 0]
+        if std_vector.shape[0] > 0:
+            mode_std = stats.mode(std_vector)[0] # can be zero if there is big signal loss
+            logging.debug(str(mode_std)+' is mode std of trace split into '+ str(stdtw)+ ' second chunks')
+            scaled = np.divide(filtered_data, mode_std)
+            return scaled
+        elif std_vector.shape[0] == 0:
+            logging.error(' File std is all 0, returning None')
+            return None
 
     def _correct_bad_messages(self):
         '''