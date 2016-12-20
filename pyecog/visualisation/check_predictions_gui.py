6458def4c5cf779e4fb02345a13f5259ad3e1918
jcornford <jonathan.cornford@gmail.com>
2016-12-20 10:46:43 +0000

    ## Improvements of check predictions gui. Now: - scrolling implemented with keys - lines display the location of predicted seizure start and ends, these can be dragged and time read off their labels
    ## Slight issues
     - the tid box is very clumsy way of doing things, should catch the error if not in predictions file? (make it be in the predicitions file in future).
     - the tree widget is a little weird too, do you need children?
     - be a bit more careful about memory usage? especially if hdf5 files get bigger, load only what you need to see and cache it, currently loading the full file, and just downsampling for display
    
     ## Still to do:
     - save the geomtry between sessions
     - dragging the lines can be used to update the predictions? and this can be exported as seizures
     - similarly predicted seizures can be ignored...

--- pyecog/visualisation/check_predictions_gui.py
+++ pyecog/visualisation/check_predictions_gui.py
@@ -3,13 +3,16 @@ import os
 import numpy as np
 import pandas as pd
 from PyQt4 import QtGui#,# uic
-from PyQt4.QtCore import QThread, SIGNAL
+from PyQt4.QtCore import QThread, SIGNAL, Qt
 import pyqtgraph as pg
 
 from  pyecog.visualisation import check_preds_design
 
 from pyecog.ndf.h5loader import H5File
-from pyecog.visualisation.pyqtgraph_playing import HDF5Plot
+#from pyecog.visualisation.pyqtgraph_playing import HDF5Plot
+
+#TODO
+# - you are currently loading the entire h5 file into memory..
 
 class PVio(QtGui.QMainWindow, check_preds_design.Ui_MainWindow):
     def __init__(self, parent=None):
@@ -27,22 +30,30 @@ class PVio(QtGui.QMainWindow, check_preds_design.Ui_MainWindow):
         self.load_preds_btn.clicked.connect(self.load_pred_file)
 
         self.plot_1 = self.GraphicsLayoutWidget.addPlot()
+        self.tid_box.setValue(6)
         #self.traceSelector.valueChanged.connect(self.plot_traces)
         #self.channel_selector.valueChanged.connect(self.plot_traces)
         self.treeWidget.itemSelectionChanged.connect(self.tree_selection)
 
-        #self.load_pred_file('/Users/jonathan/PhD/Data/PV_dendritic_intergration/Data/2016_05/2016_05_11/2016_05_11_slice1_cell1_cf2.ASC')
-    def tree_selection(self):
-        "this is messy"
+        #self.plot_1.scene().sigMouseMoved.connect(self.print_mouse_position)
+        #self.mouseline = pg.InfiniteLine(pos = None)
+        #self.plot_1.addItem(self.mouseline, ignoreBounds = True)
+        #self.coords_label = pg.TextItem()
+        #self.plot_1.addItem(self.coords_label)
 
+        self.debug_load_pred_files()
+    def tree_selection(self):
+        "grab tree detail and use to plot"
+
+        seizure_buffer = 5 # seconds either side of seizure to plot
         current_item = self.treeWidget.currentItem()
         if current_item.text(0).endswith('.h5'):
             root = current_item
-            fields = current_item.child(0)
+            fields = current_item.parent()
         else:
             fields = current_item
-            root = current_item.parent()
-            print(fields.text(0))
+            root = current_item.child(0)
+            #print(fields.text(0))
         tid = int(float(fields.text(3)))
         start = int(float(fields.text(1)))
         end = int(float(fields.text(2)))
@@ -59,9 +70,13 @@ class PVio(QtGui.QMainWindow, check_preds_design.Ui_MainWindow):
         hdf5_plot = HDF5Plot(parent = self.plot_1, viewbox = self.bx1)
         hdf5_plot.setHDF5(data_dict['data'], data_dict['time'], self.fs)
         self.plot_1.addItem(hdf5_plot)
-        self.plot_1.addItem(pg.PlotCurveItem(data_dict['time'], data_dict['data']))
+        lpen = pg.mkPen((255,0,0), width=2, style= Qt.DashLine)
+        self.plot_1.addItem(pg.InfiniteLine(pos=start, pen =lpen, movable=True,label='{value:0.2f}',
+                       labelOpts={'position':0.1, 'color': (200,200,100), 'fill': (200,200,200,50), 'movable': True}))
+        self.plot_1.addItem(pg.InfiniteLine(pos=end, pen = lpen, movable=True,label='{value:0.2f}',
+                       labelOpts={'position':0.1, 'color': (200,200,100), 'fill': (200,200,200,50), 'movable': True}))
 
-        self.plot_1.setXRange(start, end)
+        self.plot_1.setXRange(start-seizure_buffer, end+seizure_buffer)
         self.plot_1.setTitle(str(index)+' - '+ root.text(0)+ '\n' + str(start)+' - ' +str(end))
         self.plot_1.setLabel('left', 'Voltage (uV)')
         self.plot_1.setLabel('bottom','Time (s)')
@@ -93,15 +108,41 @@ class PVio(QtGui.QMainWindow, check_preds_design.Ui_MainWindow):
         index =  row['Index']
         start =  row['Start']
         end = row['End']
-        item = QtGui.QTreeWidgetItem([str(filename)])
+         # flipped these two to read better
+        fname_entry = [str(filename)]
+        details_entry = [str(index), str(start), str(end),str(tids[0])] # bad, should make only having one explcit
+        item = QtGui.QTreeWidgetItem(details_entry)
         item.setFirstColumnSpanned(True)
 
-        for tid in tids:
-            #string_ =
-            item.addChild(QtGui.QTreeWidgetItem([str(index), str(start), str(end),str(tid)]))
+
+        item.addChild(QtGui.QTreeWidgetItem(fname_entry))
         self.tree_items.append(item)
 
         self.treeWidget.addTopLevelItems(self.tree_items)
+
+    def print_mouse_position(self, pos):
+        mousepoint = self.plot_1.getViewBox().mapSceneToView(pos)
+        print(mousepoint)
+        self.mouseline.setPos(mousepoint.x())
+        #self.coords_label.setText("<span style='font-size: 6pt'> x=%0.1f <div style='text-align: center'> , <span style='font-size: 6pt''color: red'>y=%0.1f</span>" % (1111,2222))
+        #self.coords_label.setText(str(mousepoint.x()))
+
+    def keyPressEvent(self, eventQKeyEvent):
+        key = eventQKeyEvent.key()
+        x,y = self.plot_1.getViewBox().viewRange()
+        if key == Qt.Key_Up:
+            self.plot_1.getViewBox().setYRange(min = y[0]*0.9, max = y[1]*0.9, padding = 0)
+        if key == Qt.Key_Down:
+            self.plot_1.getViewBox().setYRange(min = y[0]*1.1, max = y[1]*1.1,padding = 0)
+        if key == Qt.Key_Right:
+            scroll_i = (x[1]-x[0])*0.05
+            self.plot_1.getViewBox().setXRange(min = x[0]+scroll_i, max = x[1]+scroll_i, padding=0)
+        if key == Qt.Key_Left:
+            scroll_i = (x[1]-x[0])*0.05
+            self.plot_1.getViewBox().setXRange(min = x[0]-scroll_i, max = x[1]-scroll_i, padding=0)
+        #if type(event) == QtGui.QKeyEvent:
+            #print(event.key())
+            #event.accept()
 
     def load_pred_file(self):
         fname = QtGui.QFileDialog.getOpenFileName(self, 'select predicitons file', self.home)
@@ -118,8 +159,27 @@ class PVio(QtGui.QMainWindow, check_preds_design.Ui_MainWindow):
 
         for i,row in list(self.predictions_df.iterrows()):
             fpath = os.path.join(self.h5directory,row['Filename'])
-            h5 = H5File(fpath)
-            tids = h5.attributes['t_ids']
+            #TODO
+            #So not bothering to load the tids here as should be one only per seizure... can either  load on demand
+            # or use only for the data explorer stuff. Can maybe have dynamic when you click to see the full tree.
+            # problem is with files with many false positives, spend time loading for too long!
+
+            # or have a button for this...
+
+            #h5 = H5File(fpath)
+            #tids = h5.attributes['t_ids']
+            tids = [self.tid_box.value()]
+            s,e = row['Start'], row['End']
+            self.populate_tree(row, tids)
+
+    def debug_load_pred_files(self): # stripped down version of the above for debugging gui code
+        self.h5directory = '/Volumes/G-DRIVE with Thunderbolt/GL_Steffan/2016_10/M6'
+        fname = '/Volumes/G-DRIVE with Thunderbolt/GL_Steffan/2016_10/clf_predictions_m6_201610_no_pks.csv'
+        self.predictions_df = pd.read_csv(fname)
+        self.predictions_df['Index'] = self.predictions_df.index
+        for i,row in list(self.predictions_df.iterrows()):
+            fpath = os.path.join(self.h5directory,row['Filename'])
+            tids = [self.tid_box.value()]
             s,e = row['Start'], row['End']
             self.populate_tree(row, tids)
 
@@ -158,6 +218,121 @@ class LoadFileThread(QThread):
         print('sup, loading: '+self.filename)
         self.load_file(self.filename)
         self.emit(SIGNAL('catch_data(PyQt_PyObject)'), self.h5obj)
+
+class HDF5Plot(pg.PlotCurveItem):
+    """
+    Create a subclass of PlotCurveItem for displaying a very large
+    data set from an HDF5 file that does not neccesarilly fit in memory.
+
+    The basic approach is to override PlotCurveItem.viewRangeChanged such that it
+    reads only the portion of the HDF5 data that is necessary to display the visible
+    portion of the data. This is further downsampled to reduce the number of samples
+    being displayed.
+
+    A more clever implementation of this class would employ some kind of caching
+    to avoid re-reading the entire visible waveform at every update.
+    """
+    def __init__(self, downsample_limit = 20000,viewbox = None, *args, **kwds):
+        " TODO what are the args and kwds for PlotCurveItem class?"
+        self.hdf5 = None
+        self.time = None
+        self.fs = None
+        self.vb = viewbox
+        self.limit = downsample_limit # maximum number of samples to be plotted, 10000 orginally
+        pg.PlotCurveItem.__init__(self, *args, **kwds)
+
+
+    def keyPressEvent(self, event):
+        if event.key() == QtCore.Qt.Key_Escape:
+            self.close()
+        else:
+            print(key)
+
+    def setHDF5(self, data, time, fs):
+        self.hdf5 = data
+        self.time = time
+        self.fs = fs
+        #print ( self.hdf5.shape, self.time.shape)
+        self.updateHDF5Plot()
+
+    def viewRangeChanged(self):
+        self.updateHDF5Plot()
+
+    def updateHDF5Plot(self):
+        if self.hdf5 is None:
+            self.setData([])
+            return
+
+        #vb = self.getViewBox()
+        #if vb is None:
+        #    return  # no ViewBox yet
+
+        # Determine what data range must be read from HDF5
+        xrange = [i*self.fs for i in self.vb.viewRange()[0]]
+        start = max(0,int(xrange[0])-1)
+        stop = min(len(self.hdf5), int(xrange[1]+2))
+
+        # Decide by how much we should downsample
+        ds = int((stop-start) / self.limit) + 1
+        if ds == 1:
+            # Small enough to display with no intervention.
+            visible_y = self.hdf5[start:stop]
+            visible_x = self.time[start:stop]
+            scale = 1
+        else:
+            # Here convert data into a down-sampled array suitable for visualizing.
+            # Must do this piecewise to limit memory usage.
+            samples = 1 + ((stop-start) // ds)
+            visible_y = np.zeros(samples*2, dtype=self.hdf5.dtype)
+            visible_x = np.zeros(samples*2, dtype=self.time.dtype)
+            sourcePtr = start
+            targetPtr = 0
+
+            # read data in chunks of ~1M samples
+            chunkSize = (1000000//ds) * ds
+            while sourcePtr < stop-1:
+
+                chunk = self.hdf5[sourcePtr:min(stop,sourcePtr+chunkSize)]
+                chunk_x = self.time[sourcePtr:min(stop,sourcePtr+chunkSize)]
+                sourcePtr += len(chunk)
+                #print(chunk.shape, chunk_x.shape)
+
+                # reshape chunk to be integral multiple of ds
+                chunk = chunk[:(len(chunk)//ds) * ds].reshape(len(chunk)//ds, ds)
+                chunk_x = chunk_x[:(len(chunk_x)//ds) * ds].reshape(len(chunk_x)//ds, ds)
+
+                # compute max and min
+                #chunkMax = chunk.max(axis=1)
+                #chunkMin = chunk.min(axis=1)
+
+                mx_inds = np.argmax(chunk, axis=1)
+                mi_inds = np.argmin(chunk, axis=1)
+                row_inds = np.arange(chunk.shape[0])
+
+                chunkMax = chunk[row_inds, mx_inds]
+                chunkMin = chunk[row_inds, mi_inds]
+                chunkMax_x = chunk_x[row_inds, mx_inds]
+                chunkMin_x = chunk_x[row_inds, mi_inds]
+
+                # interleave min and max into plot data to preserve envelope shape
+                visible_y[targetPtr:targetPtr+chunk.shape[0]*2:2] = chunkMin
+                visible_y[1+targetPtr:1+targetPtr+chunk.shape[0]*2:2] = chunkMax
+                visible_x[targetPtr:targetPtr+chunk_x.shape[0]*2:2] = chunkMin_x
+                visible_x[1+targetPtr:1+targetPtr+chunk_x.shape[0]*2:2] = chunkMax_x
+
+                targetPtr += chunk.shape[0]*2
+
+            visible_x = visible_x[:targetPtr]
+            visible_y = visible_y[:targetPtr]
+            #print('**** now downsampling')
+            #print(visible_y.shape, visible_x.shape)
+            scale = ds * 0.5
+
+        # TODO: setPos, scale, resetTransform methods... scale?
+        self.setData(visible_x, visible_y) # update the plot
+        #self.setPos(start, 0) # shift to match starting index ### Had comment out to stop it breaking... when limit is >0?!
+        self.resetTransform()
+        #self.scale(scale, 1)  # scale to match downsampling
 
 
 def main():
